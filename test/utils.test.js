00d527a62fa3b3c254ce272b8b80017670cc0b6b
João Gabriel Vilela <jgvilela@gmail.com>
2021-07-14 14:59:27 -0300

    todos os testes feitos e encontrado as possibilidades de bugs

--- test/utils.test.js
+++ test/utils.test.js
@@ -15,10 +15,52 @@ describe('Utils', () => {
         });
     });
 
+    describe('gerarNumeroAleatorio', () => {
+        test('inicio nao pode ser maior que o fim', () => {
+            expect(gerarNumeroAleatorio(1, 0))
+                .toBe(-1);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('numero aleatorio gerado pelo metodo (maior que)', () => {
+            expect(gerarNumeroAleatorio(0, 10))
+                .toBeGreaterThanOrEqual(0);
+        });
+    });
+
+    describe('gerarNumeroAleatorio', () => {
+        test('numero aleatorio gerado pelo metodo (menor que)', () => {
+            expect(gerarNumeroAleatorio(0, 10))
+                .toBeLessThanOrEqual(10);
+        });
+    });
+
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
-    });    
+    }); 
+    
+    describe('acharCaracter', () => {
+        test('comprimento fornecido diferente do comprimento real', () => {
+            expect(acharCaracter(16, 'o cavalo da coroa', 'c'))
+                .toBe('comprimento fornecido diferente do comprimento real');
+        });
+    }); 
+    
+    describe('acharCaracter', () => {
+        test('caracter encontrado', () => {
+            expect(acharCaracter(17, 'o cavalo da coroa', 'c'))
+                .toBe(2);
+        });
+    }); 
+
+    describe('acharCaracter', () => {
+        test('caracter nao encontrado', () => {
+            expect(acharCaracter(17, 'o cavalo da coroa', 'w'))
+                .toBe('caracter nao esta presente na cadeia de string');
+        });
+    }); 
 });
